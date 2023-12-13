37f3a8f2a33e02fd753a4a079ff2247add492f16
Hacker-Code-J <hacker3740@gmail.com>
2023-12-13 15:42:25 +0900

    Laptop231213v1.1

--- views/linked_list/script.js
+++ views/linked_list/script.js
@@ -1,4 +1,3 @@
-// script.js
 class Node {
     constructor(data) {
         this.data = data;
@@ -9,16 +8,16 @@ class Node {
 class LinkedList {
     constructor() {
         this.head = null;
+        this.canvas = document.getElementById("canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.memoryCells = [];
+        this.arrowX = 50;
+        this.cellWidth = 60;
+        this.cellHeight = 40;
+        this.arrowSpacing = 20;
     }
 
-    insertAtStart(data) {
-        const newNode = new Node(data);
-        newNode.next = this.head;
-        this.head = newNode;
-        this.updateVisualization();
-    }
-
-    insertAtEnd(data) {
+    append(data) {
         const newNode = new Node(data);
         if (!this.head) {
             this.head = newNode;
@@ -29,109 +28,47 @@ class LinkedList {
             }
             current.next = newNode;
         }
-        this.updateVisualization();
+        this.visualize();
     }
 
-    deleteNode(data) {
+    visualize() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         let current = this.head;
-        let prev = null;
+        let cellX = this.arrowX;
+        while (current) {
+            // Draw memory cell
+            this.ctx.fillStyle = "#3498db";
+            this.ctx.fillRect(cellX, 50, this.cellWidth, this.cellHeight);
+            this.memoryCells.push({ x: cellX, y: 50 });
 
-        while (current && current.data !== data) {
-            prev = current;
+            // Draw arrow
+            if (current.next) {
+                this.drawArrow(cellX + this.cellWidth + this.arrowSpacing, 70, this.arrowSpacing - 10);
+            }
+
+            // Draw data inside cell
+            this.ctx.fillStyle = "white";
+            this.ctx.font = "16px Arial";
+            this.ctx.fillText(current.data, cellX + 20, 75);
+
+            cellX += this.cellWidth + this.arrowSpacing;
             current = current.next;
         }
-
-        if (!current) return;
-
-        if (!prev) {
-            this.head = current.next;
-        } else {
-            prev.next = current.next;
-        }
-        this.updateVisualization();
     }
 
-    updateVisualization() {
-        // Data preparation
-        let data = [];
-        let current = this.head;
-        while (current != null) {
-            data.push(current.data);
-            current = current.next;
-        }
-
-        // Select the SVG container
-        const svg = d3.select("#linked-list svg");
-        const nodeRadius = 20;
-        const nodeSpacing = 50;
-
-        // Bind data to circles
-        const nodes = svg.selectAll("circle").data(data);
-        
-        // Enter new nodes
-        nodes.enter().append("circle")
-            .attr("r", nodeRadius)
-            .attr("cx", (d, i) => i * nodeSpacing + nodeRadius)
-            .attr("cy", nodeRadius)
-            .style("fill", "lightblue")
-            .style("stroke", "black");
-
-        // Update existing nodes
-        nodes.attr("cx", (d, i) => i * nodeSpacing + nodeRadius);
-
-        // Remove old nodes
-        nodes.exit().remove();
-
-        // Bind data to lines (for next pointers)
-        const links = svg.selectAll("line").data(data.slice(0, -1));
-        
-        // Enter new lines
-        links.enter().append("line")
-            .attr("x1", (d, i) => i * nodeSpacing + nodeRadius * 2)
-            .attr("y1", nodeRadius)
-            .attr("x2", (d, i) => (i + 1) * nodeSpacing)
-            .attr("y2", nodeRadius)
-            .style("stroke", "black");
-
-        // Update existing lines
-        links.attr("x2", (d, i) => (i + 1) * nodeSpacing);
-
-        // Remove old lines
-        links.exit().remove();
+    drawArrow(x, y, length) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x + length, y - 10);
+        this.ctx.lineTo(x + length, y + 10);
+        this.ctx.closePath();
+        this.ctx.fillStyle = "#3498db";
+        this.ctx.fill();
     }
 }
 
-// Instantiate the linked list
-const list = new LinkedList();
-
-// Event listeners for buttons
-document.getElementById('insert-start').addEventListener('click', () => openModal('insertAtStart'));
-document.getElementById('insert-end').addEventListener('click', () => openModal('insertAtEnd'));
-document.getElementById('delete-node').addEventListener('click', () => openModal('deleteNode'));
-
-// Handle modal interactions
-function openModal(action) {
-    const modal = document.getElementById('data-input-modal');
-    modal.style.display = 'block';
-
-    document.getElementById('submit-data').onclick = function() {
-        const data = parseInt(document.getElementById('node-data').value);
-        if (!isNaN(data)) {
-            list[action](data);
-        }
-        modal.style.display = 'none';
-    };
-}
-
-// Close modal on click outside
-window.onclick = function(event) {
-    const modal = document.getElementById('data-input-modal');
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Close modal on close button click
-document.getElementsByClassName("close-button")[0].onclick = function() {
-    document.getElementById('data-input-modal').style.display = "none";
-}
+const linkedList = new LinkedList();
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.append(40);
